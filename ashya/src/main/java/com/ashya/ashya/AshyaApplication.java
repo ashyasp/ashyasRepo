7b587d8bb1d399e6ca780eb9d7b3c7d05df64fe7
Ashyasp <ashyas427@gmail.com>
2024-06-26 13:23:55 -0400

    ashya

---
+++ ashya/src/main/java/com/ashya/ashya/AshyaApplication.java
@@ -0,0 +1,13 @@
+package com.ashya.ashya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshyaApplication.class, args);
+	}
+
+}
---
+++ ashya/src/test/java/com/ashya/ashya/AshyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashya.ashya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
